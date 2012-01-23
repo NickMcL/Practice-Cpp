0570cecf4b3d31532c161181be89626a95aee05d
Nick McLaughlin <n.mclaughlin22@yahoo.com>
2012-01-23 08:32:20 -0500

    changed hello.C and added *.swp to .gitignore

--- hello.C
+++ hello.C
@@ -4,8 +4,13 @@
 int main()
 {
 	using namespace std;
-	cout << "Come up and C++ me some time.";
-	cout << endl;
-	cout << "You won't regret it!" << endl;
+
+	int carrots;
+
+	cout << "How many carrots do you have?" << endl;
+	cin >> carrots;
+	cout << "Here are two more. ";
+	carrots = carrots + 2;
+	cout << "Now you have " << carrots << " carrots." << endl;
 	return 0;
 }
